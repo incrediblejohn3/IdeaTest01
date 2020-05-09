62865e772a0f0e8ec24fcd5d30738d31046f6829
incrediblejohn3 <252820813@qq.com>
2020-05-09 09:18:28 +0800

    5.8.1

---
+++ src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.pojo;
+
+import com.sun.tracing.dtrace.ArgsAttributes;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private String sex;
+    private int age;
+
+}
