c9dd32e939d5237b14a51eb0df25dfe0759bf481
incrediblejohn3 <252820813@qq.com>
2020-05-11 10:05:15 +0800

    5.11 aop

---
+++ src/main/java/com/example/demo/LogAOP.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+@Component
+public class LogAOP {
+    @Around(value = "execution(* com.example.demo.service..*.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object object = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        long runTime = endTime - startTime;
+        String targetName = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("类名："+targetName);
+        System.out.println("方法名："+methodName);
+        System.out.println("执行时间："+runTime);
+        return object;
+    }
+}
